1f65d22beff41ea93b20a52a75418816298eb0c1
Harrish S <harrish.s@hotmail.ca>
2024-04-29 23:27:30 -0400

    create sol faucet

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -3,11 +3,13 @@ import { useEffect } from "react";
 import { useConnection } from "@solana/wallet-adapter-react";
 import * as web3 from "@solana/web3.js";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
 	const { connection } = useConnection();
-	const { publicKey } = useWallet();
+	const { publicKey, sendTransaction } = useWallet();
 	const [balance, setBalance] = useState(0);
+	const [txnSig, setTxnSig] = useState("");
 
 	useEffect(() => {
 		const getInfo = async () => {
@@ -21,11 +23,53 @@ const Dashboard = () => {
 		getInfo();
 	}, [connection, publicKey]);
 
+	const fundWallet = async (event) => {
+		event.preventDefault();
+
+		if (!publicKey || !connection) {
+			toast.error("Wallet not connected");
+			return;
+		}
+
+		const sender = web3.Keypair.generate();
+
+		await connection.requestAirdrop(
+			sender.publicKey,
+			web3.LAMPORTS_PER_SOL * 1
+		);
+
+		const transaction = new web3.Transaction().add(
+			web3.SystemProgram.transfer({
+				fromPubkey: sender.publicKey,
+				toPubkey: publicKey,
+				lamports: web3.LAMPORTS_PER_SOL * 1,
+			})
+		);
+
+		try {
+			const signature = await sendTransaction(transaction, connection, {
+				signers: [sender],
+			});
+			setTxnSig(signature);
+		} catch (error) {
+			toast.error("Transaction failed");
+		}
+	};
+
 	return (
 		<>
 			<h1>Dashboard</h1>
 			<p>Wallet: {publicKey?.toBase58()}</p>
 			<p>Balance: {balance} SOL</p>
+			<form onSubmit={(event) => fundWallet(event)}>
+				<button type="submit">Airdrop 1 Sol</button>
+			</form>
+			<p>
+				Link to Txn:{" "}
+				{txnSig
+					? `https://explorer.solana.com/tx/${txnSig}?cluster=devnet`
+					: "No transaction yet"}
+			</p>
 		</>
 	);
 };
