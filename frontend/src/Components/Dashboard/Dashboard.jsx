7c8913502db56749e0fb5b99ac1ade2c2fe598c9
Harrish S <harrish.s@hotmail.ca>
2024-04-30 15:22:21 -0400

    can send sol to different wallet

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -10,6 +10,8 @@ const Dashboard = () => {
 	const { publicKey, sendTransaction } = useWallet();
 	const [balance, setBalance] = useState(0);
 	const [txnSig, setTxnSig] = useState("");
+	const [acct, setAcct] = useState("");
+	const [amount, setAmount] = useState(0);
 
 	useEffect(() => {
 		const getInfo = async () => {
@@ -21,11 +23,10 @@ const Dashboard = () => {
 			}
 		};
 		getInfo();
-	}, [connection, publicKey]);
+	}, [connection, publicKey, balance]);
 
+	//Code for Solana Faucet
 	const fundWallet = async (event) => {
-		event.preventDefault();
-
 		if (!publicKey || !connection) {
 			toast.error("Wallet not connected");
 			return;
@@ -55,20 +56,65 @@ const Dashboard = () => {
 			toast.error("Transaction failed");
 		}
 	};
+	//End of Code for Solana Faucet
+
+	//Code for sending Solana
+	const handleTxn = async () => {
+		if (!publicKey || !connection) {
+			toast.error("Wallet not connected");
+			return;
+		}
+
+		const transaction = new web3.Transaction();
+		const instruction = web3.SystemProgram.transfer({
+			fromPubkey: publicKey,
+			lamports: web3.LAMPORTS_PER_SOL * amount,
+			toPubkey: acct,
+		});
+
+		transaction.add(instruction);
+
+		try {
+			const signature = await sendTransaction(transaction, connection);
+			setTxnSig(signature);
+
+			const newBalance = balance - amount;
+			setBalance(newBalance);
+		} catch (error) {
+			toast.error("Transaction failed");
+		}
+	};
 
 	return (
 		<>
 			<h1>Dashboard</h1>
 			<p>Wallet: {publicKey?.toBase58()}</p>
 			<p>Balance: {balance} SOL</p>
-			<form onSubmit={(event) => fundWallet(event)}>
-				<button type="submit">Airdrop 1 Sol</button>
-			</form>
+			<button onClick={fundWallet}>Airdrop 1 Sol</button>
+			<input
+				type="text"
+				placeholder="Address of Reciever"
+				onChange={(event) => setAcct(event.target.value)}
+			/>
+			<input
+				type="number"
+				min={0}
+				placeholder="Amount of Sol to Send"
+				onChange={(event) => setAmount(event.target.value)}
+			/>
+			<button onClick={handleTxn}>Send Sol</button>
 			<p>
 				Link to Txn:{" "}
-				{txnSig
-					? `https://explorer.solana.com/tx/${txnSig}?cluster=devnet`
-					: "No transaction yet"}
+				{txnSig ? (
+					<a
+						href={`https://explorer.solana.com/tx/${txnSig}?cluster=devnet`}
+						target="_blank"
+					>
+						{txnSig}
+					</a>
+				) : (
+					"No transaction yet"
+				)}
 			</p>
 		</>
 	);
